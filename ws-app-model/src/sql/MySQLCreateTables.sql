63fc619474e40627ac552769b0bde7c5ed955544
Diego <diegoseoanegonzalez8@gmail.com>
2020-11-03 20:13:35 +0100

    SQL script added

--- ws-app-model/src/sql/MySQLCreateTables.sql
+++ ws-app-model/src/sql/MySQLCreateTables.sql
@@ -1,3 +1,39 @@
 -- ----------------------------------------------------------------------------
 -- Model
 -------------------------------------------------------------------------------
+
+DROP TABLE Race;
+DROP TABLE Inscriptions;
+
+-- --------------------------------- Race -------------------------------------
+CREATE TABLE Race(
+    raceId BIGINT NOT NULL AUTO_INCREMENT,
+    maxParticipants INT NOT NULL,
+    description VARCHAR(1024) COLLATE latin1_bin,
+    price FLOAT NOT NULL,
+    raceDate DATETIME NOT NULL,
+    city VARCHAR(64) NOT NULL,
+    creationDate DATETIME NOT NULL,
+    participants INT NOT NULL,
+    CONSTRAINT RacePK PRIMARY KEY(raceId),
+    CONSTRAINT validMaxParticipants CHECK (maxParticipants > 0),
+    CONSTRAINT validParticipants CHECK (participants >= 0),
+    CONSTRAINT validPrice CHECK (price >= 0)
+) ENGINE = InnoDB;
+
+-- ----------------------------- Inscriptions ---------------------------------
+CREATE TABLE Inscriptions(
+    inscriptionId BIGINT NOT NULL AUTO_INCREMENT,
+    raceId BIGINT NOT NULL,
+    mail VARCHAR(64) NOT NULL,
+    creditCardNumber VARCHAR(32),
+    reservationDate DATETIME NOT NULL,
+    dorsal INT NOT NULL,
+    dorsalCollected BOOLEAN NOT NULL,
+    price FLOAT NOT NULL,
+    CONSTRAINT InscriptionPK PRIMARY KEY(inscriptionId),
+    CONSTRAINT InscriptionRaceFK FOREIGN KEY(raceId)
+        REFERENCES Race(raceId) ON DELETE CASCADE,
+    CONSTRAINT validPrice CHECK (price >= 0),
+    CONSTRAINT validDorsal CHECK (dorsal >= 0)
+) ENGINE = InnoDB;
