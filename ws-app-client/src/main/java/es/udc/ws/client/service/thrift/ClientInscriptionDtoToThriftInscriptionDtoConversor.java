64f85f0d455d8fec2f5a0961809608d10b4921d4
Diego Seoane <diegoseoanegonzalez8@gmail.com>
2021-02-05 19:17:01 +0100

    [Comun] Added clients conversors for Thrift

--- ws-app-client/src/main/java/es/udc/ws/client/service/thrift/ClientInscriptionDtoToThriftInscriptionDtoConversor.java
+++ ws-app-client/src/main/java/es/udc/ws/client/service/thrift/ClientInscriptionDtoToThriftInscriptionDtoConversor.java
@@ -1,4 +1,25 @@
 package es.udc.ws.client.service.thrift;
 
+import es.udc.ws.client.service.dto.ClientInscriptionDto;
+import es.udc.ws.races.thrift.ThriftInscriptionDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClientInscriptionDtoToThriftInscriptionDtoConversor {
+
+    public static List<ClientInscriptionDto> toClientInscriptionDto(List<ThriftInscriptionDto> inscriptions){
+        List<ClientInscriptionDto> dtos = new ArrayList<>(inscriptions.size());
+
+        for(ThriftInscriptionDto inscription : inscriptions){
+            dtos.add(toClientInscriptionDto(inscription));
+        }
+
+        return dtos;
+    }
+
+    public static ClientInscriptionDto toClientInscriptionDto(ThriftInscriptionDto inscription){
+        return new ClientInscriptionDto(inscription.getInscriptionId(), inscription.getRaceId(), inscription.getMail(),
+                inscription.getCreditCardNumber(), inscription.getDorsal(), inscription.isDorsalCollected());
+    }
 }
--- ws-app-client/src/main/java/es/udc/ws/client/service/thrift/ClientRaceDtoToThriftRaceDtoConversor.java
+++ ws-app-client/src/main/java/es/udc/ws/client/service/thrift/ClientRaceDtoToThriftRaceDtoConversor.java
@@ -1,4 +1,34 @@
 package es.udc.ws.client.service.thrift;
 
+import es.udc.ws.client.service.dto.ClientRaceDto;
+import es.udc.ws.races.thrift.ThriftRaceDto;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClientRaceDtoToThriftRaceDtoConversor {
+
+    public static ThriftRaceDto toThriftRaceDto(ClientRaceDto race){
+        return new ThriftRaceDto(race.getRaceId(), race.getMaxParticipants(), race.getDescription(),
+                race.getInscriptionPrice(), race.getRaceDate().toString(), race.getRaceLocation(),
+                race.getParticipants());
+    }
+
+    public static List<ClientRaceDto> toClientRaceDto(List<ThriftRaceDto> races){
+        List<ClientRaceDto> dtos = new ArrayList<>(races.size());
+
+        for (ThriftRaceDto race : races){
+            dtos.add(toClientRaceDto(race));
+        }
+
+        return dtos;
+    }
+
+    public static ClientRaceDto toClientRaceDto(ThriftRaceDto race){
+        return new ClientRaceDto(race.getRaceId(), race.getMaxParticipants(), race.getDescription(),
+                Double.valueOf(race.getInscriptionPrice()).floatValue(), LocalDateTime.parse(race.getRaceDate()),
+                race.getRaceLocation(), race.getParticipants());
+    }
+
 }
