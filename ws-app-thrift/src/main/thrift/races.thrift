f07740f32d37f4161a0269933ac7ad5688914cf0
Diego Seoane <diegoseoanegonzalez8@gmail.com>
2021-02-05 18:28:35 +0100

    [Comun] Added races IDL for Thrift

---
+++ ws-app-thrift/src/main/thrift/races.thrift
@@ -0,0 +1,61 @@
+namespace java es.udc.ws.races.thrift
+
+struct ThriftRaceDto {
+    1: i64 raceId;
+    2: i32 maxParticipants;
+    3: string description;
+    4: double inscriptionPrice;
+    5: string raceDate;
+    6: string raceLocation;
+    7: i32 participants
+}
+
+struct ThriftInscriptionDto {
+    1: i64 inscriptionId;
+    2: i64 raceId;
+    3: string mail;
+    4: string creditCardNumber;
+    5: i32 dorsal;
+    6: bool dorsalCollected;
+}
+
+exception ThriftInputValidationException {
+    1: string message
+}
+
+exception ThriftInstanceNotFoundException {
+    1: string instanceId
+    2: string instanceType
+}
+
+exception ThriftAlreadyCollectedException {
+    1: i64 inscriptionId
+    2: string mail
+}
+
+exception ThriftIncorrectCreditCardException {
+    1: i64 inscriptionId
+    2: string mail
+}
+
+service ThriftRaceService {
+
+    i64 addRace(1: ThriftRaceDto raceDto)
+        throws (1: ThriftInputValidationException e)
+
+    ThriftRaceDto findRace (1: i64 raceId)
+        throws (1: ThriftInstanceNotFoundException e)
+
+    list<ThriftRaceDto> findRaces(1: string date, string city)
+        throws (1: ThriftInputValidationException e)
+
+    i64 addInscription(1: i64 raceId, 2: string mail, 3: string creditCard)
+        throws (1: ThriftInputValidationException e1, 2: ThriftInstanceNotFoundException e2)
+
+    list<ThriftInscriptionDto> findInscriptions(1: string mail) throws (1: ThriftInputValidationException e)
+
+    i32 collectDorsal(1: string creditCard, 2: i64 inscriptionId)
+        throws (1: ThriftInputValidationException e1, 2: ThriftInstanceNotFoundException e2,
+                3: ThriftAlreadyCollectedException e3, 4: ThriftIncorrectCreditCardException e4)
+
+}
